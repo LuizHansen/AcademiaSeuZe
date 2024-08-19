a16e01c46249d2e6632efd0933dd26690a2b3f39
LuizHansen <luizhansen751@gmail.com>
2024-08-19 10:11:47 -0300

    Adicionar arquivos de projeto.

---
+++ AcademiaZe/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcademiaZe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not null)
+            {
+                framePrincipal.Content = null;
+            }
+        }
+        private void ButtonColaborador_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not PageCadastrarColaborador)
+            {
+                framePrincipal.Content = new PageCadastrarColaborador();
+            }
+        }
+        private void ButtonAluno_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not PageCadastrarAluno)
+            {
+                framePrincipal.Content = new PageCadastrarAluno();
+            }
+        }
+        private void ButtonMatricula_Click(object sender, RoutedEventArgs e)
+        {
+            if (framePrincipal.Content is not PageCadastrarMatricula)
+            {
+                framePrincipal.Content = new PageCadastrarMatricula();
+            }
+        }
+        private void ButtonFrequencia_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not PageCadastrarFrequencia)
+            {
+                framePrincipal.Content = new PageCadastrarFrequencia();
+            }
+        }
+        private void ButtonAvaliacao_Click(object sender, RoutedEventArgs e)
+        {
+            if (framePrincipal.Content is not PageCadastrarAvaliacao)
+            {
+                framePrincipal.Content = new PageCadastrarAvaliacao();
+            }
+        }
+
+        private void ButtonLogradouro_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not PageCadastrarLogradouro)
+            {
+                framePrincipal.Content = new PageCadastrarLogradouro();
+            }
+        }
+        private void ButtonSenha_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not PageCadastrarSenha)
+            {
+                framePrincipal.Content = new PageCadastrarSenha();
+            }
+        }
+
+        private void ButtonLoginLogoff_Click(object sender, RoutedEventArgs e)
+        {
+            if(framePrincipal.Content is not PageLoginLogoff)
+            {
+                framePrincipal.Content = new PageLoginLogoff();
+            }
+        }
+
+    }
+}
---
+++ AcademiaZe/PageCadastrarMatricula.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcademiaZe
+{
+    /// <summary>
+    /// Interação lógica para PageCadastrarMatricula.xam
+    /// </summary>
+    public partial class PageCadastrarMatricula : Page
+    {
+        public PageCadastrarMatricula()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
