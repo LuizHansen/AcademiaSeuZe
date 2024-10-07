045d895d4dfab41cae85a910160d210009223264
LuizHansen <luizhansen751@gmail.com>
2024-10-07 20:31:37 -0300

    tio lu resolveu

--- AcademiaZe/Funcoes.cs
+++ AcademiaZe/Funcoes.cs
@@ -19,14 +19,14 @@ namespace AcademiaZe
         {
             // pt-BR, en-US, es-ES
             // ? indica que o valor pode ser nulo
-            string? auxIdiomaRegiao = ConfigurationManager.AppSettings.Get("IdiomaRegiao");
+            //string? auxIdiomaRegiao = ConfigurationManager.AppSettings.Get("IdiomaRegiao");
             // no ternário estamos tratando para isso não acontecer
-            string idiomaRegiao = (auxIdiomaRegiao is not null) ? auxIdiomaRegiao : "";
+            //string idiomaRegiao = (auxIdiomaRegiao is not null) ? auxIdiomaRegiao : "";
             // Definir a cultura e ajusta o idioma/região
             // o operador ! (null-forgiving) afirma que o valor já foi tratado e não será nulo aqui
-            CultureInfo culture = new(idiomaRegiao!);
-            Thread.CurrentThread.CurrentUICulture = culture;
-            Thread.CurrentThread.CurrentCulture = culture;
+            //CultureInfo culture = new(idiomaRegiao!);
+            //Thread.CurrentThread.CurrentUICulture = culture;
+            //Thread.CurrentThread.CurrentCulture = culture;
         }
         public static void GanhaFoco(object sender)
         {
